c930ae035265017c2a0ffdc127127a29c3d368c0
deninthomas <deninthomas@outlook.com>
2021-12-29 01:26:14 +0530

    Product Page

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Navigation from './common/Navigation';
+import Product from './components/product/Product';
 //import Home from './components/home/Home';
 
 
+// ReactDOM.render(
+// <BrowserRouter><Navigation/></BrowserRouter>,
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
-<BrowserRouter><Navigation/></BrowserRouter>,
-  document.getElementById('root')
-);
+  <BrowserRouter><Product/></BrowserRouter>,
+    document.getElementById('root')
+  );
